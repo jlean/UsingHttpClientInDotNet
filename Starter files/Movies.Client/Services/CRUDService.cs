0667574162df1711440a8a3d95566e24d88e75b7
unknown <Junior>
2021-09-30 15:59:50 -0400

    set accepted format at method level not globaly (as done with httpClient)

--- Starter files/Movies.Client/Services/CRUDService.cs
+++ Starter files/Movies.Client/Services/CRUDService.cs
@@ -20,14 +20,14 @@ namespace Movies.Client.Services
             _httpClient.BaseAddress = new Uri("http://localhost:57863");
             _httpClient.Timeout = new TimeSpan(0, 0, 30);
             _httpClient.DefaultRequestHeaders.Clear();
-            _httpClient.DefaultRequestHeaders.Accept.Add(
+           /* _httpClient.DefaultRequestHeaders.Accept.Add(
                 new MediaTypeWithQualityHeaderValue("application/json"));
             _httpClient.DefaultRequestHeaders.Accept.Add(
-               new MediaTypeWithQualityHeaderValue("application/xml", 0.9));
+               new MediaTypeWithQualityHeaderValue("application/xml", 0.9));*/
         }
         public async Task Run()
         {
-            await GetResource();
+            await GetResourceThroughHttpRequestMessage();
         }
 
         public async Task GetResource()
@@ -50,6 +50,22 @@ namespace Movies.Client.Services
                 var serializer = new XmlSerializer(typeof(List<Movie>));
                 movies = (List<Movie>)serializer.Deserialize(new StringReader(content));
             }
+        }
+
+        public async Task GetResourceThroughHttpRequestMessage()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "api/movies");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            var movies = JsonSerializer.Deserialize<IEnumerable<Movie>>(content,
+                new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                });
         }
     }
 }
