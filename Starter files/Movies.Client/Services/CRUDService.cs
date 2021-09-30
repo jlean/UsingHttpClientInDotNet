6952e8ea629406959f6474edda15f1d8ce06fa01
unknown <Junior>
2021-09-30 15:32:17 -0400

    serializing json and xml working

--- Starter files/Movies.Client/Services/CRUDService.cs
+++ Starter files/Movies.Client/Services/CRUDService.cs
@@ -1,15 +1,55 @@
-﻿using System;
+﻿using Movies.Client.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Movies.Client.Services
 {
     public class CRUDService : IIntegrationService
     {
+        private static HttpClient _httpClient = new HttpClient();
+
+        public CRUDService()
+        {
+            _httpClient.BaseAddress = new Uri("http://localhost:57863");
+            _httpClient.Timeout = new TimeSpan(0, 0, 30);
+            _httpClient.DefaultRequestHeaders.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            _httpClient.DefaultRequestHeaders.Accept.Add(
+               new MediaTypeWithQualityHeaderValue("application/xml"));
+        }
         public async Task Run()
         {
-        
+            await GetResource();
+        }
+
+        public async Task GetResource()
+        {
+            var response = await _httpClient.GetAsync("api/movies");
+            response.EnsureSuccessStatusCode();
+
+            var movies = new List<Movie>();
+            var content = await response.Content.ReadAsStringAsync();
+            if (response.Content.Headers.ContentType.MediaType == "application/json")
+            {
+                movies = JsonSerializer.Deserialize<List<Movie>>(content,
+                    new JsonSerializerOptions()
+                    {
+                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                    });
+            }
+            else if (response.Content.Headers.ContentType.MediaType == "application/xml")
+            {
+                var serializer = new XmlSerializer(typeof(List<Movie>));
+                movies = (List<Movie>)serializer.Deserialize(new StringReader(content));
+            }
         }
     }
 }
